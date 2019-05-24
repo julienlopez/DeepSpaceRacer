efde014cb713b98e9a223faa6d7de3e32543dd3d
julien lopez <julien.lopez51@gmail.com>
2019-05-24 16:36:32 +0200

    Update build.sh

--- travis-scripts/build.sh
+++ travis-scripts/build.sh
@@ -6,10 +6,4 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make unit_tests gui
-echo "."
-ls -l .
-echo "bin"
-ls -l bin
-find . -name unit_tests
-bin/unit_tests
-
+./unit_tests/bin/unit_tests
