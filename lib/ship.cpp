e207d6012407c1d51efd9acf6cf41ddc81946dcf
julien lopez <julien.lopez51@gmail.com>
2019-05-26 14:19:45 +0200

    add some basic building blocks to using a ship

--- lib/ship.cpp
+++ lib/ship.cpp
@@ -1,11 +1,17 @@
 #include "ship.hpp"
 
-Ship::Ship(const Units::Mass dry_mass)
-    : m_dry_mass(dry_mass)
+Ship::Ship(ThrusterContainer_t thrusters, const Units::Mass dry_mass)
+    : m_thrusters(std::move(thrusters))
+    , m_dry_mass(dry_mass)
 {
 }
 
 Units::Mass Ship::mass() const
 {
     return m_dry_mass;
+}
+
+std::size_t Ship::numbeOfThrusters() const
+{
+    return m_thrusters.size();
 }
--- lib/ship.hpp
+++ lib/ship.hpp
@@ -2,15 +2,26 @@
 
 #include "units.hpp"
 
+struct Thruster
+{
+    const Units::Force full_thrust;
+    double throttle;
+};
+
 class Ship
 {
 public:
-    Ship(const Units::Mass dry_mass = 1000 * boost::units::si::kilograms);
+    using ThrusterContainer_t = std::vector<Thruster>;
+
+    Ship(ThrusterContainer_t thrusters, const Units::Mass dry_mass = 1000 * boost::units::si::kilograms);
 
     ~Ship() = default;
 
     Units::Mass mass() const;
-    
+
+	std::size_t numbeOfThrusters() const;
+
 private:
+    const ThrusterContainer_t m_thrusters;
     const Units::Mass m_dry_mass;
 };
