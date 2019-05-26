a156941aa3aceed2a403357f3e5cee54bf44873c
julien lopez <julien.lopez51@gmail.com>
2019-05-26 14:31:02 +0200

    trying to fix build on travis-ci

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# git clone https://github.com/julienlopez/ETL
-# git clone https://github.com/Microsoft/GSL
-
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
-sudo apt-get install -y cmake gcc-9 g++-9 libboost-all-dev lcov
+sudo apt-get install -y cmake gcc-8 g++-8 libboost-all-dev lcov
 # sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake lcov
 
-sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-9 90
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-8 80
