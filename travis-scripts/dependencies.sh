df87a57dcab2c65ca5bad30805fcadf707d2c27b
julien lopez <julien.lopez51@gmail.com>
2019-05-26 17:07:20 +0200

    Update dependencies.sh

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
-sudo apt-get install -y cmake gcc-8 g++-8 libboost-all-dev lcov
+sudo apt-get install -y cmake gcc-8 g++-8 lcov
 # sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake lcov
 
 sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-8 80
