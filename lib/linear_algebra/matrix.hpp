30d13588160f6b9c520c6711d01914ed6b93df18
julien lopez <julien.lopez51@gmail.com>
2019-06-04 20:55:45 +0200

    improving vectors a bit

--- lib/linear_algebra/matrix.hpp
+++ lib/linear_algebra/matrix.hpp
@@ -9,10 +9,19 @@ template <class T, std::size_t NbRows, std::size_t NbColumns> class Matrix
 {
 public:
     using Storage_t = std::array<T, NbRows * NbColumns>;
+    using Index = std::size_t;
 
     Matrix(Storage_t values) noexcept
         : m_values(std::move(values))
     {
+    }
+
+    /**
+     * @brief vector element access
+     */
+    std::enable_if_t<NbRows == 1 || NbColumns == 1, const T&> operator()(const Index index) const
+    {
+        return m_values[index];
     }
 
     template <class U>
