eeb92597bfb9de56e2be54d5993e8bea44089451
julien lopez <julien.lopez51@gmail.com>
2019-06-24 22:54:13 +0200

    adding some basic one thruster force computation tests

--- unit_tests/test_ship.cpp
+++ unit_tests/test_ship.cpp
@@ -16,11 +16,35 @@ TEST_CASE("Test Ship with only one thruster", "[engine]")
 
     SECTION("Basic ship with a thruster pointing straight up")
     {
-        Ship ship({{Thruster{1 * newtons}, {{0 * meters, -1 * meters}}, pi / 4 * radians}}, 1000 * kilograms);
-        CHECK(ship.numbeOfThrusters() == 1);
+        Ship ship({{Thruster{1 * newtons}, {{0 * meters, -1 * meters}}, pi / 2 * radians}}, 1000 * kilograms);
+        REQUIRE(ship.numbeOfThrusters() == 1);
         ship.setThrust(0, 0.);
         CHECK(ship.thrust() == Vector2d<Units::Force>{{0 * newtons, 0 * newtons}});
         ship.setThrust(0, 1.);
         CHECK(ship.thrust() == Vector2d<Units::Force>{{0 * newtons, 1 * newtons}});
+    }
+
+    SECTION("Basic ship with a thruster pointing straight down")
+    {
+        Ship ship({{Thruster{1 * newtons}, {{0 * meters, 1 * meters}}, 3 * pi / 2 * radians}}, 1000 * kilograms);
+        REQUIRE(ship.numbeOfThrusters() == 1);
+        ship.setThrust(0, 1.);
+        CHECK(ship.thrust() == Vector2d<Units::Force>{{0 * newtons, -1 * newtons}});
+    }
+
+    SECTION("Basic ship with a thruster pointing straight left")
+    {
+        Ship ship({{Thruster{1 * newtons}, {{-1 * meters, 0 * meters}}, 0 * radians}}, 1000 * kilograms);
+        REQUIRE(ship.numbeOfThrusters() == 1);
+        ship.setThrust(0, 1.);
+        CHECK(ship.thrust() == Vector2d<Units::Force>{{1 * newtons, 0 * newtons}});
+    }
+
+    SECTION("Basic ship with a thruster pointing straight right")
+    {
+        Ship ship({{Thruster{1 * newtons}, {{1 * meters, 0 * meters}}, pi * radians}}, 1000 * kilograms);
+        REQUIRE(ship.numbeOfThrusters() == 1);
+        ship.setThrust(0, 1.);
+        CHECK(ship.thrust() == Vector2d<Units::Force>{{-1 * newtons, 0 * newtons}});
     }
 }
